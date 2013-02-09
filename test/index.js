bb3af27dc8bedb0896381a6caf4350c9cfb8b29a
Eran Hammer <eran@hueniverse.com>
2013-02-09 13:49:45 -0800

    Simple agent support

--- test/index.js
+++ test/index.js
@@ -37,12 +37,17 @@ describe('Scooter', function () {
         };
 
         var server = new Hapi.Server();
+        server.route({method: 'GET', path: '/', handler: function () {
+
+            return this.reply(this.plugins.scooter.os.family);
+        }});
 
         server.plugin().register(plugin, options, function (err) {
 
             expect(err).to.not.exist;
-            server.inject({ method: 'GET', url: '/no-found' }, function (res) {
+            server.inject({ method: 'GET', url: '/', headers: { 'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3' } }, function (res) {
 
+                expect(res.result).to.equal('iOS');
                 done();
             });
         });
