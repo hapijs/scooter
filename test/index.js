c55c8299c9b88c05d0da7c766a6c5f8b4b108981
Adam Bretz <arbretz@gmail.com>
2014-09-29 11:17:16 -0400

    Removed outer describe.

--- test/index.js
+++ test/index.js
@@ -18,24 +18,21 @@ var it = lab.it;
 var expect = Lab.expect;
 
 
-describe('Scooter', function () {
+it('parses and sets user-agent information for an incoming request', function (done) {
 
-    it('parses and sets user-agent information for an incoming request', function (done) {
+    var server = new Hapi.Server();
+    server.route({method: 'GET', path: '/', handler: function (request, reply) {
 
-        var server = new Hapi.Server();
-        server.route({method: 'GET', path: '/', handler: function (request, reply) {
+        return reply(request.plugins.scooter.os.family);
+    }});
 
-            return reply(request.plugins.scooter.os.family);
-        }});
+    server.pack.register({ plugin: Scooter }, function (err) {
 
-        server.pack.register({ plugin: Scooter }, function (err) {
+        expect(err).to.not.exist;
+        server.inject({ method: 'GET', url: '/', headers: { 'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3' } }, function (res) {
 
-            expect(err).to.not.exist;
-            server.inject({ method: 'GET', url: '/', headers: { 'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3' } }, function (res) {
-
-                expect(res.result).to.equal('iOS');
-                done();
-            });
+            expect(res.result).to.equal('iOS');
+            done();
         });
     });
 });
