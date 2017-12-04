96163afc26ac6c875b98d1b56820b9917a640ed1
Daniel Bretoi <danielb2@users.noreply.github.com>
2017-12-03 16:48:16 -0800

    Hapi17 (#37)
    * 4.0.0
    
    * update for hapi17
    
    * stricter semver requirement. export pkg. remove unneeded await. remove unneeded async declaration
    
    * add package-lock to gitignore

--- test/index.js
+++ test/index.js
@@ -23,23 +23,19 @@ const expect = Code.expect;
 
 describe('scooter', () => {
 
-    it('parses and sets user-agent information for an incoming request', (done) => {
+    it('parses and sets user-agent information for an incoming request', async () => {
 
         const server = new Hapi.Server();
-        server.connection();
-        server.route({ method: 'GET', path: '/', handler: (request, reply) => {
+        server.route({ method: 'GET', path: '/', handler: (request, h) => {
 
-            return reply(request.plugins.scooter.os.family);
+            return request.plugins.scooter.os.family;
         } });
 
-        server.register(Scooter, (err) => {
+        await server.register(Scooter);
 
-            expect(err).to.not.exist();
-            server.inject({ method: 'GET', url: '/', headers: { 'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3' } }, (res) => {
+        server.inject({ method: 'GET', url: '/', headers: { 'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3' } }, (res) => {
 
-                expect(res.result).to.equal('iOS');
-                done();
-            });
+            expect(res.result).to.equal('iOS');
         });
     });
 });
